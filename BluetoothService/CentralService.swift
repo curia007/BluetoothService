4d92ae37581ffcdf90e2b11069ebee5b45023a7f
Carmelo Uria <curia@carmelouria.com>
2019-08-11 16:56:37 -0600

    Updated

--- BluetoothService/CentralService.swift
+++ BluetoothService/CentralService.swift
@@ -9,6 +9,14 @@
 import Foundation
 import CoreBluetooth
 
+import os.log
+
+struct BTConstants {
+    // These are sample GATT service strings. Your accessory will need to include these services/characteristics in its GATT database
+    static let sampleServiceUUID = CBUUID(string: "AAAA")
+    static let sampleCharacteristicUUID = CBUUID(string: "BBBB")
+}
+
 internal class CentralService : NSObject
 {
     fileprivate let bluetoothDispatchQueue : DispatchQueue = DispatchQueue(label: "com.carmelouria.services.bluetooth", qos: .utility, attributes: .concurrent, autoreleaseFrequency: .inherit, target: nil)
@@ -34,7 +42,29 @@ extension CentralService : CBCentralManagerDelegate
 {
     func centralManagerDidUpdateState(_ central: CBCentralManager)
     {
-        
+       switch central.state {
+       case .resetting:
+           os_log("Connection with the system service was momentarily lost. Update imminent")
+       case .unsupported:
+           os_log("Platform does not support the Bluetooth Low Energy Central/Client role")
+       case .unauthorized:
+           switch central.authorization {
+           case .restricted:
+               os_log("Bluetooth is restricted on this device")
+           case .denied:
+               os_log("The application is not authorized to use the Bluetooth Low Energy role")
+           default:
+               os_log("Something went wrong. Cleaning up cbManager")
+           }
+       case .poweredOff:
+           os_log("Bluetooth is currently powered off")
+       case .poweredOn:
+           os_log("Starting central Manager")
+           let matchingOptions = [CBConnectionEventMatchingOption.serviceUUIDs: [BTConstants.sampleServiceUUID]]
+           central.registerForConnectionEvents(options: matchingOptions)
+       default:
+           os_log("Cleaning up cbManager")
+       }
     }
     
     func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?)
