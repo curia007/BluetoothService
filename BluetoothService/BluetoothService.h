56e0d277742129b54b4105d7e098688204e9d69a
Carmelo Uria <curia@carmelouria.com>
2019-08-11 14:53:18 -0600

    Initial Commit

---
+++ BluetoothService/BluetoothService.h
@@ -0,0 +1,19 @@
+//
+//  BluetoothService.h
+//  BluetoothService
+//
+//  Created by Carmelo Uria on 8/11/19.
+//  Copyright © 2019 Carmelo Uria Corporation. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BluetoothService.
+FOUNDATION_EXPORT double BluetoothServiceVersionNumber;
+
+//! Project version string for BluetoothService.
+FOUNDATION_EXPORT const unsigned char BluetoothServiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BluetoothService/PublicHeader.h>
+
+
